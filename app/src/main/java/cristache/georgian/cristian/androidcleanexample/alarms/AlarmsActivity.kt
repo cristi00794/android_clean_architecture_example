6c494dad97bb82450b71b22ec9d24d6967b1865c
Cristache Georgian Cristian <cristian.cristache@itom.ro>
2020-07-13 16:43:03 +0300

    - adding transitions to fragments

--- app/src/main/java/cristache/georgian/cristian/androidcleanexample/alarms/AlarmsActivity.kt
+++ app/src/main/java/cristache/georgian/cristian/androidcleanexample/alarms/AlarmsActivity.kt
@@ -1,20 +1,37 @@
 package cristache.georgian.cristian.androidcleanexample.alarms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import cristache.georgian.cristian.androidcleanexample.R
 
+
 class AlarmsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alarms_activity)
 
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
         val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.alarmsFragment)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) ||
+                super.onSupportNavigateUp()
     }
 }
